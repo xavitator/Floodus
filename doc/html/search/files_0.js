e5dfa8ec43117db93b8357a37c2f0a1f36058725
Xavitator <xavier75013@gmail.com>
2019-03-05 18:39:47 +0100

    sup doc et bin via gitignore

--- doc/html/search/files_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['main_2ec',['Main.c',['../df/d09/Main_8c.html',1,'']]]
-];
--- doc/html/search/functions_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['main',['main',['../df/d09/Main_8c.html#ae66f6b31b5ad750f1fe042a706a4e3d4',1,'Main.c']]]
-];
