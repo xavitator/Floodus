79bd6da28070025f1e9d4e6863df9e2f3c61c591
Xavitator <xavier75013@gmail.com>
2019-03-26 12:40:23 +0100

    ajout flag de debug

--- include/debug.h
+++ include/debug.h
@@ -1,18 +1,18 @@
 #ifndef H_DEBUG
 #define H_DEBUG
 
-#define DEBUG 1 
+#define DEBUG 1
 
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <errno.h>
 
-void debug(char *name, const char *msg);
-void debug_and_exit(char *name, const char *msg, int exit_code);
-void debug_hex(char *name, uint8_t *data,int length);
-void debug_hex_and_exit(char *name, uint8_t *data,int length, int exit_code);
-void debug_int(char *name, int rc);
-void debug_int_and_exit(char *name, int rc, int exit_code);
+void debug(uint8_t flag, char *name, const char *msg);
+void debug_and_exit(uint8_t flag, char *name, const char *msg, int exit_code);
+void debug_hex(uint8_t flag, char *name, uint8_t *data, int length);
+void debug_hex_and_exit(uint8_t flag, char *name, uint8_t *data, int length, int exit_code);
+void debug_int(uint8_t flag, char *name, int rc);
+void debug_int_and_exit(uint8_t flag, char *name, int rc, int exit_code);
 
 #endif
--- src/debug.c
+++ src/debug.c
@@ -3,43 +3,52 @@
 /**
  * @brief
  * Message de debug général
+ * @param flag flag de debug de la fonction appelante
  * @param name le nom de la constante ou la chaine vide
  * @param msg le message à afficher
  */
-void debug (char * name, const char *msg) {
-  if (DEBUG) {
-    fprintf(stderr, "[DEBUG] Str %s : %s\n",name, msg);
+void debug(uint8_t flag, char *name, const char *msg)
+{
+  if (DEBUG && flag)
+  {
+    fprintf(stderr, "[DEBUG] Str %s : %s\n", name, msg);
   }
 }
 
 /**
  * @brief
  * Message de debug général et quitte
+ * @param flag flag de debug de la fonction appelante
  * @param name le nom de la chaine
  * @param msg le message
  * @param exit_code le code d'erreur
  */
-void debug_and_exit(char * name, const char *msg, int exit_code) {
-  if (DEBUG) {
-    fprintf(stderr, "[DEBUG] Str %s : %s\n",name, msg);
+void debug_and_exit(uint8_t flag, char *name, const char *msg, int exit_code)
+{
+  if (DEBUG && flag)
+  {
+    fprintf(stderr, "[DEBUG] Str %s : %s\n", name, msg);
     exit(exit_code);
   }
 }
 
-
 /**
  * @brief
  * Affiche une requête sous forme hexadécimale
+ * @param flag flag de debug de la fonction appelante
  * @param name le nom de la requête
  * @param data le tableau d'octets
  * @param length la taille des données
  */
-void debug_hex(char *name, uint8_t *data, int length) {
-  if (DEBUG) {
+void debug_hex(uint8_t flag, char *name, uint8_t *data, int length)
+{
+  if (DEBUG && flag)
+  {
     fprintf(stderr, "[DEBUG] Hexa %s : ", name);
-    for (int i = 0 ; i < length ; i++) {
+    for (int i = 0; i < length; i++)
+    {
       fprintf(stderr, "%.2x ", data[i]);
-    } 
+    }
     fprintf(stderr, "\n");
   }
 }
@@ -47,17 +56,21 @@ void debug_hex(char *name, uint8_t *data, int length) {
 /**
  * @brief
  * Affiche une requête sous forme hexadécimale et quitte
+ * @param flag flag de debug de la fonction appelante
  * @param name le nom de la requête
  * @param data le tableau d'octets
  * @param length la taille des données
  * @param exit_code le code d'erreur
  */
-void debug_hex_and_exit (char *name, uint8_t *data, int length, int exit_code) {
-  if (DEBUG) {
+void debug_hex_and_exit(uint8_t flag, char *name, uint8_t *data, int length, int exit_code)
+{
+  if (DEBUG && flag)
+  {
     fprintf(stderr, "[DEBUG] Hexa %s : ", name);
-    for (int i = 0 ; i < length ; i++) {
+    for (int i = 0; i < length; i++)
+    {
       fprintf(stderr, "%.2x ", data[i]);
-    } 
+    }
     fprintf(stderr, "\n");
     exit(exit_code);
   }
@@ -66,27 +79,31 @@ void debug_hex_and_exit (char *name, uint8_t *data, int length, int exit_code) {
 /**
  * @brief
  * Affiche un int
+ * @param flag flag de debug de la fonction appelante
  * @param name le nom de l'int
  * @param rc l'int à afficher
  */
-void debug_int(char *name, int rc) {
-  if(DEBUG) {
-    fprintf(stderr, "[DEBUG] Int %s: %d\n",name, rc);
+void debug_int(uint8_t flag, char *name, int rc)
+{
+  if (DEBUG && flag)
+  {
+    fprintf(stderr, "[DEBUG] Int %s: %d\n", name, rc);
   }
 }
 
 /**
  * @brief
  * Affiche un int et quitte
+ * @param flag flag de debug de la fonction appelante
  * @param name le nom de l'int
  * @param rc l'int
  * @param exit_code le code d'erreur
  */
-void debug_int_and_exit(char *name, int rc, int exit_code) {
-  if(DEBUG) {
-    fprintf(stderr, "[DEBUG] Int %s : %d\n",name, rc);
+void debug_int_and_exit(uint8_t flag, char *name, int rc, int exit_code)
+{
+  if (DEBUG && flag)
+  {
+    fprintf(stderr, "[DEBUG] Int %s : %d\n", name, rc);
     exit(exit_code);
   }
 }
-
-
