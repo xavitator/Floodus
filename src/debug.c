91a61939277f4fec6b3214d2a32cabffffc471d5
Kolibs <etienne@marais.green>
2019-03-26 09:25:13 +0100

    Comment debug

--- src/debug.c
+++ src/debug.c
@@ -1,11 +1,24 @@
 #include "debug.h"
 
+/**
+ * @brief
+ * Message de debug général
+ * @param name le nom de la constante ou la chaine vide
+ * @param msg le message à afficher
+ */
 void debug (char * name, const char *msg) {
   if (DEBUG) {
     fprintf(stderr, "[DEBUG] Str %s : %s\n",name, msg);
   }
 }
 
+/**
+ * @brief
+ * Message de debug général et quitte
+ * @param name le nom de la chaine
+ * @param msg le message
+ * @param exit_code le code d'erreur
+ */
 void debug_and_exit(char * name, const char *msg, int exit_code) {
   if (DEBUG) {
     fprintf(stderr, "[DEBUG] Str %s : %s\n",name, msg);
@@ -13,6 +26,14 @@ void debug_and_exit(char * name, const char *msg, int exit_code) {
   }
 }
 
+
+/**
+ * @brief
+ * Affiche une requête sous forme hexadécimale
+ * @param name le nom de la requête
+ * @param data le tableau d'octets
+ * @param length la taille des données
+ */
 void debug_hex(char *name, uint8_t *data, int length) {
   if (DEBUG) {
     fprintf(stderr, "[DEBUG] Hexa %s : ", name);
@@ -23,6 +44,14 @@ void debug_hex(char *name, uint8_t *data, int length) {
   }
 }
 
+/**
+ * @brief
+ * Affiche une requête sous forme hexadécimale et quitte
+ * @param name le nom de la requête
+ * @param data le tableau d'octets
+ * @param length la taille des données
+ * @param exit_code le code d'erreur
+ */
 void debug_hex_and_exit (char *name, uint8_t *data, int length, int exit_code) {
   if (DEBUG) {
     fprintf(stderr, "[DEBUG] Hexa %s : ", name);
@@ -34,12 +63,25 @@ void debug_hex_and_exit (char *name, uint8_t *data, int length, int exit_code) {
   }
 }
 
+/**
+ * @brief
+ * Affiche un int
+ * @param name le nom de l'int
+ * @param rc l'int à afficher
+ */
 void debug_int(char *name, int rc) {
   if(DEBUG) {
     fprintf(stderr, "[DEBUG] Int %s: %d\n",name, rc);
   }
 }
 
+/**
+ * @brief
+ * Affiche un int et quitte
+ * @param name le nom de l'int
+ * @param rc l'int
+ * @param exit_code le code d'erreur
+ */
 void debug_int_and_exit(char *name, int rc, int exit_code) {
   if(DEBUG) {
     fprintf(stderr, "[DEBUG] Int %s : %d\n",name, rc);
