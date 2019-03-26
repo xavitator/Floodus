c229eb9e45af11b193f4065723dd2ff934e9c381
Xavitator <xavier75013@gmail.com>
2019-03-26 21:33:02 +0100

    ajout debug mode iovec

--- include/iovec.h
+++ include/iovec.h
@@ -1,7 +1,17 @@
 #ifndef _IOVEC_H
 #define _IOVEC_H
 
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <sys/types.h>
 #include <sys/uio.h>
+
+#include "debug.h"
+
+#define D_IOVEC 0
 
 void freeiovec(struct iovec *data);
 struct iovec *copy_iovec(struct iovec *data);
