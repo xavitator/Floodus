ab318ebbd80767cd519474d445575f3237245933
Xavitator <xavier75013@gmail.com>
2019-03-30 12:18:09 +0100

    ajout commentaire de compréhension dans main

--- src/Main.c
+++ src/Main.c
@@ -25,17 +25,30 @@
 
 #define D_MAIN 1
 
+/**
+ * @brief Envoie de hello court à un destinataire contenu dans un addrinfo
+ * 
+ * @param p destinataire
+ * @return int boolean disant si tout s'est bien passé
+ */
 int make_demand(struct addrinfo *p)
 {
     data_t *hs = hello_short(myid);
     ip_port_t ipport = {0};
     ipport.port = ((struct sockaddr_in6 *)p->ai_addr)->sin6_port;
     memmove(ipport.ipv6, &((struct sockaddr_in6 *)p->ai_addr)->sin6_addr, sizeof(ipport.ipv6));
-    send_tlv(&ipport, hs, 1);
+    int rc = send_tlv(&ipport, hs, 1);
     freeiovec(hs);
-    return 0;
+    return rc;
 }
 
+/**
+ * @brief On récupère toutes les infos via getaddrinfo sur la destination et le port passé en arguments.
+ * 
+ * @param dest nom dns de la destination
+ * @param port port de la destination
+ * @return int '0' si ca s'est bien passé, '-1' sinon.
+ */
 int send_hello(char *dest, char *port)
 {
     struct addrinfo h = {0};
@@ -46,17 +59,45 @@ int send_hello(char *dest, char *port)
     h.ai_flags = AI_V4MAPPED | AI_ALL;
     rc = getaddrinfo(dest, port, &h, &r);
     if (rc < 0)
-        debug_and_exit(D_MAIN, 1, "rc", gai_strerror(rc), 1);
-    struct addrinfo *p = r;
-    while (p != NULL)
     {
-        make_demand(p);
-        p = p->ai_next;
+        debug(D_MAIN, 1, "send_hello -> rc", gai_strerror(rc));
+        return -1;
     }
+    struct addrinfo *p = r;
+
+    // demande à toutes les interfaces détectées
+    // while (p != NULL)
+    // {
+    //     make_demand(p);
+    //     p = p->ai_next;
+    // }
+    // fin de la demande à toutes les interfaces
+
+    // demande à la première interface
+    if (p == NULL)
+    {
+        debug(D_MAIN, 1, "send_hello", "aucune interface détectée pour cette adresse");
+        return -1;
+    }
+    make_demand(p);
+    // fin de la demande à la première interface
+
+    freeaddrinfo(r);
     debug(D_MAIN, 0, "send_hello", "demande effectuée pour getaddrinfo");
     return 0;
 }
 
+/**
+ * @brief initialisation du serveur.
+ * Le commande de lancement peut prendre 2 arguments.
+ * Si les deux arguments sont présents simultanémant :
+ * le premier argument correspondra au nom dns de la destination
+ * le deuxième argument correspondra au port de la destination
+ * 
+ * @param argc nombre d'arguments de la commande
+ * @param argv tableau des arguments de la commande
+ * @return int valeur de retour
+ */
 int main(int argc, char *argv[])
 {
     char *port = "1212";
@@ -83,8 +124,9 @@ int main(int argc, char *argv[])
     }
     init_sender();
     init_neighbors();
-    send_hello(default_dest, port);
-    launch_program();
+    rc = send_hello(default_dest, port);
+    if (rc >= 0)
+        launch_program();
     free_neighbors();
     return 0;
 }
