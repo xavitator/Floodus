d9cd744e774a23c03e5920d5859e3c2cb06f6fc7
Xavitator <xavier75013@gmail.com>
2019-04-17 22:42:07 +0200

    ajout commentaire

--- src/view.c
+++ src/view.c
@@ -1,9 +1,15 @@
 #include "view.h"
 
-void print_data(u_int8_t *content, u_int8_t contentlen)
+/**
+ * @brief Fonction d'affichage d'un message donnée en argument. Cette fonction n'utilise pas de fonctions de debug.
+ * 
+ * @param content contenu du message (peut ne pas se terminer par '\0')
+ * @param content_len taille du contenu
+ */
+void print_data(u_int8_t *content, u_int8_t content_len)
 {
     printf("\e[1;34m[DATA]\e[0mAffichage du contenu :\n");
-    for (u_int8_t i = 0; i < contentlen; i++)
+    for (u_int8_t i = 0; i < content_len; i++)
     {
         printf("%c", content[i]);
     }
